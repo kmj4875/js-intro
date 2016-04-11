4cb08e9ded6eefc653ccd9c6e71686db054c080b
kmj4875 <minjae.kim@wustl.edu>
2016-04-11 18:38:58 -0500

    Update scripts.js

--- 03_paragraph/scripts.js
+++ 03_paragraph/scripts.js
@@ -3,3 +3,4 @@ alert("Can you get this message to appear?");
 document.write("<h2>My first JavaScript program</h2>");
 document.write("Paragraph 1");
 document.write("Paragraph 2 ");
+document.write("Paragraph 3  ");
