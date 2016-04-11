311c50f411550a319114a6ebc92278439df145bb
kmj4875 <minjae.kim@wustl.edu>
2016-04-11 18:40:13 -0500

    Update scripts.js

--- 03_paragraph/scripts.js
+++ 03_paragraph/scripts.js
@@ -2,5 +2,5 @@ alert("Help me fix this program!");
 alert("Can you get this message to appear?");
 document.write("<h2>My first JavaScript program</h2>");
 document.write("Paragraph 1");
-document.write("Paragraph 2 ");
-document.write("Paragraph 3  ");
+document.write("Paragraph 2");
+document.write("Paragraph 3");
